fd99f833c8f6f078cd49f62bbee99104e1394778
Morten Broesby-Olsen <mortenbroesby@gmail.com>
2021-03-05 19:43:04 +0100

    chore(project): only export LRUCache by default

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,5 @@ export * from './LRUCache';
 export * from './LRUCacheNode';
 
 import { LRUCache } from './LRUCache';
-import { LRUCacheNode } from './LRUCacheNode';
 
-export default {
-  LRUCache,
-  LRUCacheNode
-};
+export default LRUCache;
