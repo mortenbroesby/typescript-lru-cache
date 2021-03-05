796b62f8d8c22b3ce279fa4650fb051271bb7ffd
Morten Broesby-Olsen <mortenbroesby@gmail.com>
2021-03-05 18:04:47 +0100

    chore(project): remove dist folder

--- dist/Game.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="screeps" />
-/**
- * Polyfill Screeps game object.
- *
- * @returns Game - Game Object with Date.now() fallback.
- */
-declare const fallbackGameInstance: () => Game;
-export { fallbackGameInstance };
-//# sourceMappingURL=Game.d.ts.map
