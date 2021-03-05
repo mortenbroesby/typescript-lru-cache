d85367c46e384ade3b7ec36b3b2e81c13f82ffb6
Morten Broesby-Olsen <mortenbroesby@gmail.com>
2021-03-05 19:56:03 +0100

    chore(export): export default

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 export * from './LRUCache';
-export * from './LRUCacheNode';
 
 import { LRUCache } from './LRUCache';
 
-export default LRUCache;
+const Cache = {
+  LRUCache
+};
+
+export default Cache;
